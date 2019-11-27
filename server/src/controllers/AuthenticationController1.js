6a22a0e2c93ce3e4093cd595ddb39ffe5cdb8862
Juan <soccer_kid21@hotmail.com>
2019-11-27 16:57:59 -0500

    added encrypted passwords and migrated existing

--- server/src/controllers/AuthenticationController1.js
+++ server/src/controllers/AuthenticationController1.js
@@ -1,22 +1,17 @@
-const crypto = require('crypto')
 const knex = require('knex')(require('./../knexfile'))
+
 module.exports = {
   // async registerUser ({ username, password }) {
-  saltHashPassword,
   async registerUser (req, res) {
     const username = req.body.username
     const email = req.body.email
     const password = req.body.password
     console.log(`Add user ${req.body.username} with password ${req.body.password}`)
-
-    const { salt, hash } = saltHashPassword(password)
-
     try {
       return knex('users').insert({
-        salt,
-        encrypted_password: hash,
         username,
-        email
+        email,
+        password
       }).then(() => {
         res.status(200).send({
           username: req.body.username,
@@ -31,16 +26,6 @@ module.exports = {
         message: 'Error: err'
       })
     }
+    // return Promise.resolve()
   }
-}; function saltHashPassword (password) {
-  const salt = randomString()
-  const hash = crypto
-    .createHmac('sha512', salt)
-    .update(password)
-  return {
-    salt,
-    hash: hash.digest('hex')
-  }
-}; function randomString () {
-  return crypto.randomBytes(4).toString('hex')
 }
