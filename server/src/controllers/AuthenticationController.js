958e288374469497c95269c1ae0349ed0ddabbf2
Juan <soccer_kid21@hotmail.com>
2019-11-27 15:57:26 -0500

    added register user to database with knex

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,12 +1,31 @@
+const knex = require('knex')(require('./../knexfile'))
+
 module.exports = {
   // async registerUser ({ username, password }) {
   async registerUser (req, res) {
+    const username = req.body.username
+    const email = req.body.email
+    const password = req.body.password
     console.log(`Add user ${req.body.username} with password ${req.body.password}`)
+    try {
+      return knex('users').insert({
+        username,
+        email,
+        password
+      }).then(() => {
+        res.status(200).send({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          message: 'Created user!'
+        })
+      })
+    } catch (err) {
+      console.log(err)
+      res.status(500).send({
+        message: 'Error: err'
+      })
+    }
     // return Promise.resolve()
-    res.status(200).send({
-      username: req.body.username,
-      password: req.body.password,
-      message: 'Created user!'
-    })
   }
 }
---
+++ server/src/migrations/20191127131919_create_users_table.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function (t) {
+    t.increments('id').primary()
+    t.string('username').notNullable()
+    t.string('email').notNullable()
+    t.string('password').notNullable()
+    t.timestamps(false, true)
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users')
+
+}
